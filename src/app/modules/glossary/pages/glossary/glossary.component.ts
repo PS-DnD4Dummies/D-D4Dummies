dc07700a92fef7410d2237f0494b650428961e56
SaraES2000 <saraes2000@gmail.com>
2024-02-27 08:59:01 +0000

    Pages principales añadidas

---
+++ src/app/modules/glossary/pages/glossary/glossary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-glossary',
+  standalone: true,
+  imports: [],
+  templateUrl: './glossary.component.html',
+  styleUrl: './glossary.component.scss'
+})
+export class GlossaryComponent {
+
+}
