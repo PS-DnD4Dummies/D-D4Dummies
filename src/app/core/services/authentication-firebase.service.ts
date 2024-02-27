851929d7a50fe4862a7732ceb52f23b406de71e1
jpere <jpereiro1@gmail.com>
2024-02-27 20:14:11 +0000

    Función de cerrar sesión

--- src/app/core/services/authentication-firebase.service.ts
+++ src/app/core/services/authentication-firebase.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, inject } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { Auth, User, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { error } from 'console';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +9,13 @@ import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from
 export class AuthenticationFirebaseService {
 
 
-  constructor(private auth:Auth) { }
+  constructor(private auth:Auth) {
+    //this.authStatusListener();
+  }
+
+  currentUser!:any ;
+  private authStatusSub = new BehaviorSubject(this.currentUser);
+  currentAuthStatus = this.authStatusSub.asObservable();
 
   //https://firebase.google.com/docs/reference/js/v8/firebase.auth.Auth#signinwithemailandpassword
   async login(email: string, password: string): Promise<any> {
@@ -32,5 +40,37 @@ export class AuthenticationFirebaseService {
       return null;
     }
   }
+
+  //https://medium.com/@anichidera/managing-auth-state-in-your-angular-firebase-app-c08d62cf3f43
+  /*USAGE:  this.auth.authStatusListener();
+            this.auth.currentAuthStatus.subscribe(authStatus => console.log(authStatus));
+  */
+  authStatusListener(){
+    onAuthStateChanged(this.auth, (user) => {
+      if (user) {
+        //const uid = user.uid;
+        this.authStatusSub.next(user);
+      } else {
+        this.authStatusSub.next(null);
+        console.log('Usuario no está registrado');
+      }
+    });
+  }
+
+  //USAGE: this.auth.signOut();
+  async signOut(){
+    try {
+      await this.auth.signOut();
+      console.log("El usuario ha cerrado sesión correctamenta");
+    } catch (error) {
+      console.log("Error al cerrar sesión. Error: " + error);
+    }
+  }
+
+
+
+    
+
+
 
 }
