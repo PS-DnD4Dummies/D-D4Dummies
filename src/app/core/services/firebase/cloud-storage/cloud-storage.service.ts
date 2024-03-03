7b1f80d9adf2b0f09772d93b1dec43d916af2990
jpere <jpereiro1@gmail.com>
2024-03-03 10:39:10 +0000

    Upload profile photo

--- src/app/core/services/firebase/cloud-storage/cloud-storage.service.ts
+++ src/app/core/services/firebase/cloud-storage/cloud-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Storage } from '@angular/fire/storage';
+import { Storage, ref } from '@angular/fire/storage';
+import { uploadBytes } from '@firebase/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,25 @@ export class CloudStorageService {
 
   constructor(private cloudStorage:Storage) { }
 
+  //https://blog.angular-university.io/angular-file-upload/
+  //Solo aceptar ficheros jpeg, jpg, png en el frontend
+  async uploadProfilePhoto(userID:string,file:File): Promise<Boolean>{
+
+    const storageRef = ref(this.cloudStorage,'profilePhotos/'+userID);
+
+    if (file.type !== "image/jpeg" && file.type !== "image/jpg" && file.type !== "image/png") {
+      return false;
+    }
   
 
+    return await uploadBytes(storageRef,file).then(()=>{
+      console.log("La foto de perfil se ha subido correctamente.");
+      return true;
+    }).catch((error)=>{
+      console.log("Error al subir la foto de perfil. Error: "+error);
+      return false;
+    });
+  }
 
 
 }
