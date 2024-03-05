b2bedfa4d624f7ca1c1c86c3e8f684be35f6d7ea
jpere <jpereiro1@gmail.com>
2024-03-05 19:06:38 +0000

    Footer implementation

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { socialMediaURL } from '@data/constanst/url';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  socialMediaURL = socialMediaURL;
 }
