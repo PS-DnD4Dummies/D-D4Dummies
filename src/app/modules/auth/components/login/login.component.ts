199e262fa60b16b4b8c26b7f946468e4422d9218
aojedanu <aojedanu@gmail.com>
2024-03-11 17:35:18 +0000

    creacion de <form> para pedir el cambio de parametros del usuario.

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { AuthenticationFirebaseService } from '@core/services/firebase/authentication/authentication-firebase.service';
+import { FormsModule } from '@angular/forms'; //para usar el form que cambia los paraametros
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
+
 export class LoginComponent {
 
 }
+
--- src/app/modules/auth/pages/profile/profile.component.ts
+++ src/app/modules/auth/pages/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthenticationFirebaseService } from '@core/services/firebase/authentication/authentication-firebase.service';
+import { FormsModule } from '@angular/forms'; //para usar el form que cambia los paraametros
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  email: string = '';
+  password: string = '';
+  newDisplayName: string = '';
+  newPhotoURL: string = '';
+
+  auth!:any;
+
+  constructor(private authService: AuthenticationFirebaseService) {
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.authService.signUp("aaaa@getMaxListeners.com", "ccccccccccc")
+    this.authService.currentAuthStatus.subscribe(auth =>this.auth = auth);
+    console.log("auhhhth");
+  }
 
 }
