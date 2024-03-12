69c70eac97c34d176a3b6915acc38538c5bd271b
aojedanu <aojedanu@gmail.com>
2024-03-12 15:13:44 +0000

    mostrar  datos del usuario

--- src/app/modules/auth/pages/profile/profile.component.ts
+++ src/app/modules/auth/pages/profile/profile.component.ts
@@ -12,6 +12,8 @@ export class ProfileComponent {
   password: string = '';
   newDisplayName: string = '';
   newPhotoURL: string = '';
+  username: string = '';
+  birthDate: string = '';
 
   auth!:any;
   selectedFileUrl!: string;
@@ -22,21 +24,40 @@ export class ProfileComponent {
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.authService.signUp("aaaa@getMaxListeners.com", "ccccccccccc")
-    this.authService.currentAuthStatus.subscribe(auth =>this.auth = auth);
+    this.authService.signUp("aaaa@getMaxListeners.com", "ccccccccccc") //ver como conseguir usuario aqui creado.
+    this.authService.currentAuthStatus.subscribe(auth =>{console.log(auth); this.auth = auth});
+    //****codigo para ver3ificar****
+    if (this.auth && this.auth.uid){ //si existe el usuario y esta autenticado.
+      this.authService.currentAuthStatus.subscribe(userInfo=> { // ver como accedee a esto da error.
+        this.username = userInfo.displayName;
+        this.birthDate = userInfo.birthDate; // no se esta mostrando
+        this.email = userInfo.email;
+      },
+        (error: any) => {
+        console.error('Error obteniendo información del usuario:', error);
+      }
+    );
+    //****codigo para ver3ificar****
+  }
+    
   }
   updateProfile(): void {
     // Llama a la función updateProfile del servicio con los parámetros necesarios
     this.authService.updateProfile(this.auth, this.newDisplayName, this.newPhotoURL);
   }
-  onFileSelected(event: { target: any; }): void {
+  onFileSelected(event: any): void {
     const input = event.target;
     const file = input.files[0];
-
+  
     if (file) {
-      // Obtener la URL de la imagen seleccionada
-      this.selectedFileUrl = URL.createObjectURL(file);
+      if (file.type === 'image/png' || file.type === 'image/jpeg' || file.type === 'image/jpg') {
+        this.selectedFileUrl = URL.createObjectURL(file);
+      } else {
+        console.error('Por favor, selecciona un archivo PNG o JPG.');
+        this.selectedFileUrl = '';
+      }
     }
   }
+
 }
 
