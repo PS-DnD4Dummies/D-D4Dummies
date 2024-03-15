155202965a314a5af186aa87ca1dc8d9063f5307
SaraES2000 <saraes2000@gmail.com>
2024-03-15 12:50:48 +0000

    Warning arreglados

--- src/app/core/services/dnd-api/dnd-api.service.ts
+++ src/app/core/services/dnd-api/dnd-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { RaceInfo } from '../../../shared/components/index';
+import { RaceInfo } from '@shared/components';
 
 
 @Injectable({
