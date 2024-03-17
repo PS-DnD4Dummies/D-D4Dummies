adeb3c32371d77dafbbdeb14dae9faec45c12330
Álvaro <alvarosacosta22@gmail.com>
2024-03-17 11:34:04 +0000

    Imágenes de header main y footer se guardan en sessionStorage tras sacarlas de cloud

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -9,30 +9,69 @@ import { FirebaseService } from '@core/services/firebase/firebase.service';
 })
 export class FooterComponent implements OnInit{
   socialMediaURL = socialMediaURL;
-  images: Map<String, String> = new Map();
+
+  insurance: Boolean;
   
-  constructor(private firebaseService:FirebaseService){}
+  constructor(private firebaseService:FirebaseService){
+    this.insurance = false;
+  }
 
   ngOnInit(): void {
     this.loadImages();
+
+    // this.cleanStorage();
   }
 
   async loadImages(){
-    this.images.clear();
-    var url = this.firebaseService.getImagesFromFile("miscPhotos/");
-    await url.then((links) => {
-      const regex = /%2F([^?]+)/;
-      var urls = links; 
-
-      for (let item of urls){
-        const match = item.match(regex);
-
-        if (match !== null){
-          this.images.set(decodeURIComponent(match[1]), item);
-        }
-      }
     
-  })
-}
+    const urlGithub = sessionStorage.getItem('github.png');
+    const urlLinkedin = sessionStorage.getItem('logotipo-de-linkedin.png');
+    const urlTwitter = sessionStorage.getItem('signo-de-twitter.png');
+    const urlInstagram = sessionStorage.getItem('instagram.png');
+
+    if (urlGithub && urlLinkedin && urlTwitter && urlInstagram) {
+
+      var image = document.querySelector('.follow-us__img--github') as HTMLImageElement;
+      image.src = urlGithub;
+
+      image = document.querySelector('.follow-us__img--linkedin') as HTMLImageElement;
+      image.src = urlLinkedin;
+
+      image = document.querySelector('.follow-us__img--twitter') as HTMLImageElement;
+      image.src = urlTwitter;
+
+      image = document.querySelector('.follow-us__img--instagram') as HTMLImageElement;
+      image.src = urlInstagram;
+      
+    } else {
+      !this.insurance ? await this.takeImagesFromCloudService(): console.log("ERROR: Casi se ejecuta un segundo intento de acceder al Cloud.");
+      this.insurance = true;
+    }
+
+  }
+
+  async takeImagesFromCloudService(){
+    var url = this.firebaseService.getImagesFromFile("miscPhotos/");
+      await url.then((links) => {
+        const regex = /%2F([^?]+)/;
+        var urls = links; 
+
+        for (let item of urls){
+          const match = item.match(regex);
+  
+          if (match !== null){
+            sessionStorage.setItem(decodeURIComponent(match[1]), JSON.stringify(item).replace(/^["']|["']$/g, ''));
+          }
+        }
+        
+    })
+
+    this.loadImages();
+  }
+
+  async cleanStorage(){
+    localStorage.clear();
+    sessionStorage.clear();
+  }
 }
 
