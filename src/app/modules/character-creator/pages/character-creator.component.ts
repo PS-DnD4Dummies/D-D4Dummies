ea789ae406eafaf0ee4be1d94c4646f6bfbbd00d
Alvaro Santana <alvarosacosta22@gmail.com>
2024-04-08 14:22:14 +0100

    avances en character creator, tema de numeros randoms

--- src/app/modules/character-creator/pages/character-creator.component.ts
+++ src/app/modules/character-creator/pages/character-creator.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { OnInit } from "@angular/core";
+import { Race, Class, Alignment, Background} from "@data/enums/enum";
 
 @Component({
     selector: 'character-creator',
@@ -9,6 +10,11 @@ import { OnInit } from "@angular/core";
 })
 
 export class CharacterCreatorComponent implements OnInit{
+    enumRace = Object.values(Race); 
+    enumClass = Object.values(Class); 
+    enumAlignment = Object.values(Alignment); 
+    enumBackground = Object.values(Background); 
+
     race: string = "";
     class: string = "";
     alignment: string = "";
@@ -19,10 +25,52 @@ export class CharacterCreatorComponent implements OnInit{
     constructor(){
     }
 
-    ngOnInit(){}
+    ngOnInit(){
+    }
+
+    enumParser(s: string){
+        return s.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    }
+
+    zaWarudo(){
+
+        this.generateRandomNumbers();
+
+        this.calculateStats();
+        this.calculatePassivePerception();
+        this.showSkills();
+        this.showProficiencies();
+        this.showTraits();
+        this.showCombat();
+
+    }
+
+    generateRandomNumbers(){
+        let numbers: number[] = [];
+        let sum = 0;
+
+        const minSum = 30;
+    
+        for (let i = 0; i < 6; i++) {
+            let num = Math.floor(Math.random() * 20) + 1;
+            numbers.push(num);
+            sum += num;
+        }
+    
+        while (sum < minSum) {
+            let index = Math.floor(Math.random() * numbers.length);
+            
+            if (numbers[index] < 20) {
+                numbers[index]++;
+                sum++;
+            }
+        }
+          
+        return numbers;
+    }
 
     calculateStats(){
-        
+
     }
 
     calculatePassivePerception(){
