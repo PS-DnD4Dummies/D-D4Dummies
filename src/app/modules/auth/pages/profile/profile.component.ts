e5a207d35e08dfd448e67759bfbc4d0660743e66
SaraES2000 <saraes2000@gmail.com>
2024-04-11 18:24:12 +0100

    Perfil modificado

--- src/app/modules/auth/pages/profile/profile.component.ts
+++ src/app/modules/auth/pages/profile/profile.component.ts
@@ -1,22 +1,18 @@
 import { Component } from '@angular/core';
 import { AuthenticationFirebaseService } from '@core/services/firebase/authentication/authentication-firebase.service';
-import { FormsModule } from '@angular/forms'; //para usar el form que cambia los paraametros
 import { FirestoreService } from '@core/services/firebase/firestore/firestore.service';
 import { FirebaseService } from '@core/services/firebase/firebase.service';
-import { CloudStorageService } from '@core/services/firebase/cloud-storage/cloud-storage.service';
-import { serverTimestamp } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { ROUTES } from '@data/constanst/routes';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
 
-  isAuthenticated: Boolean = false;
+  isAuthenticated: boolean = false;
   username: string = '';
   email: string = '';
   birthDate!: Date;
@@ -24,79 +20,70 @@ export class ProfileComponent {
 
   newUsername: string = '';
   newEmail: string = '';
-  newPassword: string = '';
   newBirthDate: string = '';
   selectedFile: any;
 
   auth: any;
-  displayImage: string = '/assets/images/default.jpg'; 
+  displayImage: string = '/assets/images/default.jpg';
   usernameError: any;
   passwordError: any;
   birthdateError: any;
 
+  isEditing: boolean = false;
+  originalProfilePhotoURL: string = '';
 
   constructor(
-    private authService: AuthenticationFirebaseService, 
-    private firestoreService : FirestoreService, 
-    private firebaseService : FirebaseService,
-    private router : Router) {
-  }
+    private authService: AuthenticationFirebaseService,
+    private firestoreService: FirestoreService,
+    private firebaseService: FirebaseService,
+    private router: Router) {}
 
   ngOnInit(): void {
-
-     this.authService.currentAuthStatus.subscribe(auth => {
-     this.isAuthenticated = !!auth; //mira si el usuario esta autenticado
+    this.authService.currentAuthStatus.subscribe(auth => {
+      this.isAuthenticated = !!auth;
       if (auth) {
-        // Usuario está iniciado sesión.
         this.auth = auth;
-        //console.log("El UID del usuario es: " + auth.uid);
-      this.firestoreService.readRealTimeUser(auth.uid).subscribe(user => {
-        this.email = user.email;
-        this.username = user.username;
-        this.birthDate = new Date(user.birthdate.seconds*1000);
-        this.profilePhotoURL = user.photoURL;
-        console.log(user)
-      });
-
+        this.firestoreService.readRealTimeUser(auth.uid).subscribe(user => {
+          this.email = user.email;
+          this.username = user.username;
+          this.birthDate = new Date(user.birthdate.seconds*1000);
+          this.profilePhotoURL = user.photoURL;
+          this.originalProfilePhotoURL = user.photoURL; // Guardar la URL original
+          console.log(user)
+        });
       }})
-    }
-  
-  signOut() {
-    this.authService.signOut();
-    this.router.navigate([ROUTES.HOME.DEFAULT]);
   }
-  
+  toggleEdit(): void {
+    this.isEditing = true;
+    this.newUsername = this.username;
+    this.newEmail = this.email;
+    this.newBirthDate = this.birthDate.toISOString().substring(0, 10);
+  }
 
-  updateProfile(): void {
+  cancelEdit(): void {
+    this.isEditing = false;
+    // Restaurar la URL original de la foto si se cancela la edición
+    this.profilePhotoURL = this.originalProfilePhotoURL;
+    // Reiniciar el archivo seleccionado
+    this.selectedFile = null;
+  }
+
+  saveChanges(): void {
     let changes : {[key:string]:any} = {};
 
-
-    if(this.newUsername != ''){
+    if (this.newUsername != '') {
       if (this.checkForValidUsername(this.newUsername)) {
         changes["username"] = this.newUsername;
       } else {
-        //this.usernameError = true;
         alert('Please enter a valid username.');
         return;
       }
     }
 
-    
-    if(this.newPassword != ''){
-      if (this.checkForValidPassword(this.newPassword)) {
-        changes["password"] = this.newPassword;
-      } else {
-        //this.passwordError = true;
-        alert('Please enter a valid password.');
-        return;
-      }
-    }
-
-    if(this.newBirthDate != ''){
+    if (this.newBirthDate != '') {
       if (this.isAdult(this.newBirthDate)) {
         changes["birthdate"] = this.newBirthDate;
       } else {
-        //this.birthDateError = true;
         alert('Please enter a valid birthdate.');
         return;
       }
@@ -106,20 +93,77 @@ export class ProfileComponent {
       changes["photoURL"] = "";
     }
 
-    //console.log(changes);
-
-    if(Object.keys(changes).length){
-      
+    if (Object.keys(changes).length) {
       this.firestoreService.readUser(this.auth.uid).then(user => {
-        if (user) { // Asegura que user no es null
+        if (user) {
+          // Actualiza el perfil del usuario en la base de datos
           this.firebaseService.updateProfileInfo(user, changes, this.selectedFile, this.auth)
+            .then(() => {
+              // Después de guardar los cambios, cierra la edición y muestra el perfil nuevamente
+              this.cancelEdit();
+            })
+            .catch(error => {
+              console.error('Error updating profile:', error);
+            });
         } else {
-          // Maneja el caso de que user sea null, si es necesario
           console.log('No se encontró el usuario.');
         }
       });
-    } 
+    } else {
+      // Si no hay cambios, simplemente cierra la edición
+      this.cancelEdit();
+    }
+  }
 
+  onFileSelected(event: any): void {
+    if (!this.isEditing) {
+      return;
+    }
+
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      const fileSizeInMB = file.size / 1024 / 1024;
+      const validExtensions = ['image/jpeg', 'image/png'];
+
+      if (!validExtensions.includes(file.type)) {
+        alert('Only .jpg and .png files are allowed.');
+        return;
+      }
+
+      if (fileSizeInMB > 10) {
+        alert('File size must be less than 10MB.');
+        return;
+      }
+
+      // Mostrar la vista previa de la imagen seleccionada
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.profilePhotoURL = reader.result as string;
+      };
+
+      this.selectedFile = file;
+    } else {
+      this.selectedFile = null;
+    }
+  }
+
+  parsePhotoGoogleURL(url: string): string {
+    const partes = url.split('=');
+    if (!url.includes("https://lh3.googleusercontent.com")) {
+      return url;
+    }
+    if (partes.length === 2 && partes[0] && partes[1]) {
+      partes[1] = 's1024';
+      return partes.join('=');
+    } else {
+      return url;
+    }
+  }
+
+  checkForValidUsername(username: string): boolean {
+    const usernamePattern = /^[a-zA-Z0-9_-]{4,20}$/;
+    return usernamePattern.test(username);
   }
 
   isAdult(birthDate: string): boolean {
@@ -131,59 +175,9 @@ export class ProfileComponent {
     return birthDateObj <= adultDate;
   }
 
-  checkForValidUsername(username: string): boolean {
-    const usernamePattern = /^[a-zA-Z0-9_-]{4,20}$/;
-    return usernamePattern.test(username);
-  }
-
   checkForValidPassword(password: string): boolean {
     return true;
     /*const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     return passwordPattern.test(password);*/
   }
-
-  onFileSelected(event: any): void {
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      const fileSizeInMB = file.size / 1024 / 1024;
-      const validExtensions = ['image/jpeg', 'image/png'];
-  
-      if (!validExtensions.includes(file.type)) {
-        alert('Only .jpg and .png files are allowed.');
-        return;
-      }
-  
-      if (fileSizeInMB > 10) {
-        alert('File size must be less than 10MB.');
-        return;
-      }
-  
-      this.selectedFile = file;
-      this.displayImage = URL.createObjectURL(this.selectedFile);
-    } else {
-      this.selectedFile = null;
-    }
-  }
-
-  parsePhotoGoogleURL(url: string): string {
-    // Dividir la URL por el signo igual
-    const partes = url.split('=');
-    
-    if(!url.includes("https://lh3.googleusercontent.com")){
-      return url;
-    }
-
-    // Verificar si se encontró el signo igual y la URL es válida
-    if (partes.length === 2 && partes[0] && partes[1]) {
-        // Modificar el tamaño del avatar a s1024
-        partes[1] = 's1024';
-        // Unir las partes nuevamente para formar la URL modificada
-        return partes.join('=');
-    } else {
-        // Si la URL no tiene el formato esperado, devolver la URL original
-        return url;
-    }
-  }
-
 }
-
