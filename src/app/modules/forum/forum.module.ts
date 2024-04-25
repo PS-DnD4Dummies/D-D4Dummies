04e1465150eeb666f6e72392c2ffcd4bd2d6c9f5
Juan Pereiro González <jpereiro1@gmail.com>
2024-04-25 13:29:58 +0100

    Creación de los modulos del foro

---
+++ src/app/modules/forum/forum.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ForumComponent } from './pages/forum/forum.component';
+
+
+
+@NgModule({
+  declarations: [ForumComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ForumModule { }
