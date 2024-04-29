c7bcd9d40ce688f7c36f50d63055335766e84d03
SaraES2000 <saraes2000@gmail.com>
2024-04-29 13:35:39 +0100

    Arreglo de ruta

--- src/app/modules/forum/forum-routing.module.ts
+++ src/app/modules/forum/forum-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainForumComponent } from './pages/main-forum/main-forum.component';
+import { ForumComponent } from './pages/forum/forum.component';
 import { TopicPageComponent } from './pages/topic-page/topic-page.component';
 
 const routes: Routes = [
-  { path: '', component: MainForumComponent },
+  { path: '', component: ForumComponent },
   { path: ':topic', component: TopicPageComponent }
 ];
 
