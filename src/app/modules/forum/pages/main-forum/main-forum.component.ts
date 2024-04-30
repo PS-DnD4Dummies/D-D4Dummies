8f7580dc73ef8561875bc8a35069a6d37b506a7c
Juan Pereiro González <jpereiro1@gmail.com>
2024-04-30 11:33:37 +0100

    Adding a overlay when user is not logged

--- src/app/modules/forum/pages/main-forum/main-forum.component.ts
+++ src/app/modules/forum/pages/main-forum/main-forum.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from '@angular/fire/auth';
+import { AuthenticationFirebaseService } from '@core/services/firebase/authentication/authentication-firebase.service';
 
 @Component({
   selector: 'app-main-forum',
@@ -8,4 +10,11 @@ import { Component } from '@angular/core';
 export class MainForumComponent {
 
   dummyList = [1, 2, 3, 4, 5];
+
+  user !: User;
+
+  constructor(private authService: AuthenticationFirebaseService){
+    this.authService.currentAuthStatus.subscribe(user => this.user=user);
+  }
+
 }
