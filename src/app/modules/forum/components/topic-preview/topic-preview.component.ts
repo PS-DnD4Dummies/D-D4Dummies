d724a2288f88dc113ea0f4451575ee587569409e
SaraES2000 <saraes2000@gmail.com>
2024-05-07 10:36:23 +0100

    Estilos con responsive topic preview hechos

--- src/app/modules/forum/components/topic-preview/topic-preview.component.ts
+++ src/app/modules/forum/components/topic-preview/topic-preview.component.ts
@@ -2,19 +2,19 @@ import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Post } from '@data/interfaces';
 
+declare var window: any;
+
 @Component({
   selector: 'app-topic-preview',
   templateUrl: './topic-preview.component.html',
   styleUrl: './topic-preview.component.scss'
 })
+
 export class TopicPreviewComponent {
 
-  constructor(private router: Router) {
-
-  }
+  constructor(private router: Router) {}
 
   @Input() post!:Post;
-
 
   goToDisplay(){
     this.router.navigate(['/forum', this.post.id]);
@@ -25,4 +25,15 @@ export class TopicPreviewComponent {
     return new Date(milliseconds);
   }
 
+  getContentPreview() {
+    if (window.innerWidth < 768) {
+      return this.post.content.length > 21 ? this.post.content.slice(0, 21) + ' ...' : this.post.content;
+    } else if (window.innerWidth >= 768 && window.innerWidth <= 1023) {
+      return this.post.content.length > 50 ? this.post.content.slice(0, 50) + ' ...' : this.post.content;
+    } else {
+      return this.post.content.length > 74 ? this.post.content.slice(0, 74) + ' ...' : this.post.content;
+    }
+  }
+
+  protected readonly window = window;
 }
